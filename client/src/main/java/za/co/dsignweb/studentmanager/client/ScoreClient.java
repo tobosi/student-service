225d3da86e64373f72859655dcc0baa4b65e2710
wandi.tobosi <tobosiw@gmail.com>
2024-06-07 18:12:54 +0200

    Fix integration with db

--- client/src/main/java/za/co/dsignweb/studentmanager/client/ScoreClient.java
+++ client/src/main/java/za/co/dsignweb/studentmanager/client/ScoreClient.java
@@ -5,6 +5,6 @@ import za.co.dsignweb.studentmanager.api.IRestCreate;
 import za.co.dsignweb.studentmanager.model.api.request.ScoreReq;
 import za.co.dsignweb.studentmanager.model.api.response.ScoreRes;
 
-@FeignClient(name = "score", url = "${student-service-base-url}/scores")
+@FeignClient(name = "score", url = "${student.service.base.url}/scores")
 public interface ScoreClient extends IRestCreate<ScoreRes, ScoreReq> {
 }
--- client/src/main/java/za/co/dsignweb/studentmanager/client/StudentClient.java
+++ client/src/main/java/za/co/dsignweb/studentmanager/client/StudentClient.java
@@ -5,6 +5,6 @@ import za.co.dsignweb.studentmanager.api.RestCrud;
 import za.co.dsignweb.studentmanager.model.api.request.StudentReq;
 import za.co.dsignweb.studentmanager.model.api.response.StudentRes;
 
-@FeignClient(name = "student", url = "${student-service-base-url}/students")
+@FeignClient(name = "student", url = "${student.service.base.url}/students")
 public interface StudentClient extends RestCrud<StudentRes,StudentReq> {
 }
